d463b1f23d5aa00c196ed8fb6f1454953cc7711a
meriem farsi <farsi.meriem60@gmail.com>
2020-08-31 20:18:25 +0000

    express_server

--- express_server.js
+++ express_server.js
@@ -15,6 +15,10 @@ app.get("/", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+app.get("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
