ec19739247ec3d9cc934db4e9833ffece16c1815
meriem farsi <farsi.meriem60@gmail.com>
2020-09-01 02:42:36 +0000

    Add second route

--- express_server.js
+++ express_server.js
@@ -19,7 +19,10 @@ app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
-
+app.get("/urls/:shortURL", (req, res) => {
+  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
+  res.render("urls_show", templateVars);
+});
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
