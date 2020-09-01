7992c174c5a5aba8b1592fad094065aef2078235
meriem farsi <farsi.meriem60@gmail.com>
2020-09-01 18:44:49 +0000

    save short url in the urlDatabase

--- express_server.js
+++ express_server.js
@@ -5,6 +5,12 @@ const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
+function generateRandomString() {
+  let r = Math.random().toString(36).substring(7);
+  //console.log("random", r);
+  return r;
+}
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -34,3 +40,13 @@ app.get("/urls.json", (req, res) => {
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  //save shortURL and longURL into urlDatabase
+  urlDatabase[generateRandomString()] = req.body.longURL;
+  console.log(urlDatabase);
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  
+});
+
+
